d43e5751086ab224d337b301b38f6c1a83ba7c44
chamodya-dilshani <chamodyaariyawansha@gmail.com>
2026-08-09 19:00:32 +0530

    add my projects

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
+// var typed = new Typed(".text", {
+//     strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+
+// Typed.js Animation
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+    strings: ["Full Stack Developer", "UI/UX Designer", "ICT Undergraduate", "Problem Solver"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
+});
+
+// Active Navigation
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.navbar a');
+
+window.addEventListener('scroll', () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.navbar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 });
